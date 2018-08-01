1c986282fdc8108444585166c8b8066374844149
TrentPearson <trent.pearson172@hotmail.com>
2018-08-01 21:32:01 +1000

    new menu system in place, and intents added too.

---
+++ Assignment1/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Content;
+
+namespace Assignment1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            ImageButton button = FindViewById<ImageButton>(Resource.Id.imageButton1);
+            button.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(Clock));
+                StartActivity(intent);
+            };
+        }
+    }
+}
+
