c173f336f97d5581e3bc7dadd0e5ddf9dd00ced6
TrentPearson <trent.pearson172@hotmail.com>
2018-08-02 12:08:25 +1000

    bug fixes to menu layout.

--- Assignment1/MainActivity.cs
+++ Assignment1/MainActivity.cs
@@ -1,22 +1,24 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-using Android.Support.V7.App;
 using Android.Content;
+using Android.Runtime;
+using Android.Views;
 
 namespace Assignment1
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            ImageButton button = FindViewById<ImageButton>(Resource.Id.imageButton1);
+            ImageButton button = FindViewById<ImageButton>(Resource.Id.imgbutton1);
             button.Click += (sender, e) =>
             {
                 var intent = new Intent(this, typeof(Clock));
